b1f0e7a8a32acec6f58347b8b4f70eff291ff38c
Sukanta Dey <sukanta.dey@slalom.com>
2022-08-24 13:24:08 -0700

    Initial Commit

---
+++ src/app/shared/payment-detail.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { PaymentDetail } from './payment-detail.model';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentDetailService {
+
+  constructor(private http: HttpClient) { }
+
+  // http://localhost:5224/api/PaymentDetail
+  
+  readonly baseURL = 'https://azurepaymentapi.azure-api.net/v1/api/PaymentDetail'
+
+  formData: PaymentDetail = new PaymentDetail();
+  list: PaymentDetail[];
+
+  postPaymentDetail() {
+    return this.http.post(this.baseURL, this.formData);
+  }
+
+  putPaymentDetail() {
+    return this.http.put(`${this.baseURL}/${this.formData.paymentDetailId}`, this.formData);
+  }
+
+  deletePaymentDetail(id: number) {
+    return this.http.delete(`${this.baseURL}/${id}`);
+  }
+
+  refreshList() {
+    this.http.get(this.baseURL)
+      .toPromise()
+      .then(res =>this.list = res as PaymentDetail[]);
+  }
+}
